01131dd97278cc1d8c7416dd9318de34e325bfd6
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-04-12 23:40:14 -0300

    Atualiza hwingui.h adiciona macro hwg_par_UINT

--- include/hwingui.h
+++ include/hwingui.h
@@ -148,3 +148,4 @@ HB_EXTERN_END
 #define hwg_par_HBITMAP(n) (HBITMAP)HB_PARHANDLE(n)
 #define hwg_par_HICON(n) (HICON)HB_PARHANDLE(n)
 #define hwg_par_HIMAGELIST(n) (HIMAGELIST)HB_PARHANDLE(n)
+#define hwg_par_UINT(n) (UINT)hb_parni(n)
