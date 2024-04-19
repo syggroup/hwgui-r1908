d6bb3effee5369fb921af4a6ca8c93d0acb4c0d8
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-04-19 14:10:58 -0300

    Atualiza hwingui.h adiciona macro hwg_par_HGDIOBJ

--- include/hwingui.h
+++ include/hwingui.h
@@ -134,6 +134,7 @@ HB_EXTERN_END
 #define hwg_par_HBITMAP(n) (HBITMAP)HB_PARHANDLE(n)
 #define hwg_par_HBRUSH(n) (HBRUSH)HB_PARHANDLE(n)
 #define hwg_par_HDC(n) (HDC)HB_PARHANDLE(n)
+#define hwg_par_HGDIOBJ(n) (HGDIOBJ)HB_PARHANDLE(n)
 #define hwg_par_HICON(n) (HICON)HB_PARHANDLE(n)
 #define hwg_par_HIMAGELIST(n) (HIMAGELIST)HB_PARHANDLE(n)
 #define hwg_par_HMENU(n) (HMENU)HB_PARHANDLE(n)
