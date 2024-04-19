cc699fedb1133f143cda2421cdab496aa45610ab
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-04-19 07:01:28 -0300

    Atualiza hwingui.h adiciona macro hwg_ret_HBRUSH

--- include/hwingui.h
+++ include/hwingui.h
@@ -151,6 +151,7 @@ HB_EXTERN_END
 #define hwg_ret_LRESULT(x) hb_retnl(x)
 //#define hwg_ret_PAINTSTRUCT(n) HB_RETHANDLE(n)
 #define hwg_ret_PAINTSTRUCT(n) hb_retptr(n)
+#define hwg_ret_HBRUSH(x) HB_RETHANDLE(x)
 
 #define hwg_vmPushUINT(x) hb_vmPushInteger(x)
 #define hwg_vmPushWPARAM(x) hb_vmPushLong(x)
