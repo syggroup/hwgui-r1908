187ccf15dc47638c7f260791f134b7e31e6993fe
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-04-23 11:50:59 -0300

    Atualiza animat.c formatação e macros

--- source/animat.c
+++ source/animat.c
@@ -9,46 +9,70 @@
 #include "hwingui.h"
 #include <commctrl.h>
 
+/*
+ANIMATE_CREATE(HWND, nId, nStyle, nX, nY, nWidth, nHeight) --> HWND
+*/
 HB_FUNC(ANIMATE_CREATE)
 {
   HWND hwnd;
-  hwnd = Animate_Create(hwg_par_HWND(1), hwg_par_UINT(2), (LONG)hb_parnl(3), GetModuleHandle(NULL));
-  MoveWindow(hwnd, hb_parnl(4), hb_parnl(5), hb_parnl(6), hb_parnl(7), TRUE);
+  hwnd = Animate_Create(hwg_par_HWND(1), hwg_par_UINT(2), hwg_par_DWORD(3), GetModuleHandle(NULL));
+  MoveWindow(hwnd, hwg_par_int(4), hwg_par_int(5), hwg_par_int(6), hwg_par_int(7), TRUE);
   hwg_ret_HWND(hwnd);
 }
 
-HB_FUNC(ANIMATE_OPEN)
+/*
+ANIMATE_OPEN(HWND, cFileName) -->
+*/
+HB_FUNC(ANIMATE_OPEN) // TODO: adicionar opção de usar 'resources'
 {
   void *hStr;
   Animate_Open(hwg_par_HWND(1), HB_PARSTR(2, &hStr, NULL));
   hb_strfree(hStr);
 }
 
+/*
+ANIMATE_PLAY(HWND, nFrom, nTo, nReplay) -->
+*/
 HB_FUNC(ANIMATE_PLAY)
 {
   Animate_Play(hwg_par_HWND(1), hwg_par_UINT(2), hwg_par_UINT(3), hwg_par_UINT(4));
 }
 
+/*
+ANIMATE_SEEK(HWND, nFrame) -->
+*/
 HB_FUNC(ANIMATE_SEEK)
 {
   Animate_Seek(hwg_par_HWND(1), hwg_par_UINT(2));
 }
 
+/*
+ANIMATE_STOP(HWND) -->
+*/
 HB_FUNC(ANIMATE_STOP)
 {
   Animate_Stop(hwg_par_HWND(1));
 }
 
+/*
+ANIMATE_CLOSE(HWND) -->
+*/
 HB_FUNC(ANIMATE_CLOSE)
 {
   Animate_Close(hwg_par_HWND(1));
 }
 
+/*
+ANIMATE_DESTROY(HWND) -->
+*/
 HB_FUNC(ANIMATE_DESTROY)
 {
   DestroyWindow(hwg_par_HWND(1));
 }
 
+/*
+ANIMATE_OPENEX(HWND, HINSTANCE, cFileName|cResource|nResource) -->
+*/
 HB_FUNC(ANIMATE_OPENEX)
 {
 #if defined(__DMC__)
